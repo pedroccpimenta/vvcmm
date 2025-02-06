f636b44e16fce06fcc45588247f8b62f55da926d
pedroccpimenta <pedroccpimenta@gmail.com>
2025-02-06 21:52:47 +0000

    :tada: update icons @ gdrive

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import openpyxl
 import datetime
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Required for session management
+#app.secret_key = "your_secret_key"  # Required for session management
 
 # Hardcoded login credentials
 
